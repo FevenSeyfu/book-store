58d20ad621ae6d145c7183dbc7b4342ad81452ff
FevenSeyfu <fevenseyfu00@gmail.com>
2023-11-21 11:18:18 +0300

    Add Routes to frontend react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+
+// pages
+import Home from './Pages/Home'
+import CreateBooks from './Pages/CreateBooks'
+import ShowBooks from './Pages/ShowBooks'
+import EditBooks from './Pages/EditBooks'
+import DeleteBooks from './Pages/DeleteBooks'
+
 
 const App = () => {
   return (
-    <div className='bg-red-400'>App</div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/books/create' element={<CreateBooks />} />
+      <Route path='/books/details/:id' element={<ShowBooks />} />
+      <Route path='/books/edit/:id' element={<EditBooks />} />
+      <Route path='/books/delete/:id' element={<DeleteBooks />} />
+    </Routes>
   )
 }
 
